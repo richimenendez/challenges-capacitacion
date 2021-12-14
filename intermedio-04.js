db8433f34fe47de1bad8c13f7b0433d207fa24d6
richimenendez <richimenen@gmail.com>
2021-12-14 17:58:41 -0600

    Optimización intermedio-04.js

--- intermedio-04.js
+++ intermedio-04.js
@@ -1,12 +1,9 @@
 function subPalindromo(s)
 {   
     let palindromo = s.charAt(0);
-    for (let i = 0; i < s.length; i++) {
+    for (let i = 0; i < s.length && s.length-1>=palindromo.length; i++) {
         const char = s.charAt(i);
-        let j = s.length-1;
-        if(s.length-i<palindromo.length)
-            return palindromo;
-        for (let j = s.length-1; j > i; j--) {
+        for (let j = s.length-1; j > i && j-i>=palindromo.length; j--) {
             const char2 = s.charAt(j);
             if(char == char2){
                 const cad = s.substring(i,j+1);
@@ -26,3 +23,6 @@ console.log(subPalindromo('babad'));
 console.log(subPalindromo('cbbd'));
 console.log(subPalindromo('a'));
 console.log(subPalindromo('ac'));
+console.log(subPalindromo('acbhdsasasasghft'));
+console.log(subPalindromo('acbhdsaasbbaabbghft'));
+console.log(subPalindromo('aaaaaaacbhdsaasbbaabbghfaaatbbbbbb'));
